9e157e9ac1257bde36cb48879157f563b0d17c2f
Aditya Patra <adityabpatra@gmail.com>
2025-09-20 20:53:42 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ Aditya Patra
 Arrays Assignment
 
 Purpose:write a program with real-world applications incorporating std::array functions
-
+This program performs and analysis of daily average temperatures during the month of January
 */
 #include <iostream>
 #include <string>
@@ -14,11 +14,13 @@ Purpose:write a program with real-world applications incorporating std::array fu
 using namespace std;
 
 int main() {
-    const int SIZE = 30;
+    const int SIZE = 31;
     ifstream fin;
     array<int, SIZE> numbers;
+    string month;
     fin.open("array_info.txt");
     if (fin.good( )) {
+        fin >> month;
         for(int i = 0; i < SIZE; i++) {
             fin >> numbers[i];
         }
